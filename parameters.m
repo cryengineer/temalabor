32c68b2155747efd44af84e323f165dec809e5ee
Balázs <cryengineer@gmail.com>
2018-10-23 08:24:14 +0200

    Add files via upload

--- parameters.m
+++ parameters.m
@@ -1,9 +1,13 @@
 Vm=400;
-w_e=2*pi*50;
-w_b=2*pi*50;
+w_e=2*pi*10;
+w_b=2*pi*10;
 Rs=0.855;
 Rr=1.15;
 Xls=0.00392*w_b;
 Xlr=0.00392*w_b;
 Xm=0.16*w_b;
 J=0.06;
+P=6;
+TL=40;
+
+Xm1=Xm*Xls*Xlr/(Xm*Xls+Xm*Xlr+Xlr*Xls);
